3b515c8e73c0316d495ef1e79204a536a326c687
alejandrapalacuos <145595611+alejandrapalacuos@users.noreply.github.com>
2024-11-14 12:04:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ if img_file_buffer is not None:
     print(prediction)
     if prediction[0][0]>0.3:
       st.header('Abriendo')
-      client1.publish("vocecita","{'gesto': 'abre la puerta'}",qos=0, retain=False)
+      client1.publish("vocecita","{'gesto': 'Abre'}",qos=0, retain=False)
       time.sleep(0.2)
     if prediction[0][1]>0.3:
       st.header('Acceso denegado')
-      client1.publish("vocecita","{'gesto': 'cierra la puerta'}",qos=0, retain=False)
+      client1.publish("vocecita","{'gesto': 'Cierra'}",qos=0, retain=False)
       time.sleep(0.2)  
