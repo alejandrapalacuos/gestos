29e0001f7bbd33bb20668828f2af20f761ca4605
alejandrapalacuos <145595611+alejandrapalacuos@users.noreply.github.com>
2024-11-14 12:11:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ if img_file_buffer is not None:
       time.sleep(0.2)
     if prediction[0][1]>0.3:
       st.header('Acceso denegado')
-      client1.publish("vocecita","{'Act1': 'cierra la puerta'}",qos=0, retain=False)
+      client1.publish("vocecita","{'Act1': 'Cierra la puerta'}",qos=0, retain=False)
       time.sleep(0.2)  
