36a2cef4970d21af5b82fffac07eb9c99f823672
alejandrapalacuos <145595611+alejandrapalacuos@users.noreply.github.com>
2024-11-14 11:32:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import paho.mqtt.client as paho
+import time
+import json
 import streamlit as st
 import cv2
 import numpy as np
@@ -5,20 +8,31 @@ import numpy as np
 from PIL import Image as Image, ImageOps as ImagOps
 from keras.models import load_model
 
-import platform
+def on_publish(client,userdata,result):             #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
 
-# Muestra la versión de Python junto con detalles adicionales
-st.write("Versión de Python:", platform.python_version())
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
+
+        
+
+
+broker="157.230.214.127"
+port=1883
+client1= paho.Client("APP_CERR")
+client1.on_message = on_message
+client1.on_publish = on_publish
+client1.connect(broker,port)
 
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-st.title("Reconocimiento de Imágenes")
-#st.write("Versión de Python:", platform.python_version())
-image = Image.open('OIG5.jpg')
-st.image(image, width=350)
-with st.sidebar:
-    st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
+st.title("Cerradura Inteligente")
+
 img_file_buffer = st.camera_input("Toma una Foto")
 
 if img_file_buffer is not None:
@@ -40,9 +54,11 @@ if img_file_buffer is not None:
     # run the inference
     prediction = model.predict(data)
     print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
-    #if prediction[0][2]>0.5:
-    # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
+    if prediction[0][0]>0.3:
+      st.header('Abriendo')
+      client1.publish("IMIA","{'gesto': 'Abre'}",qos=0, retain=False)
+      time.sleep(0.2)
+    if prediction[0][1]>0.3:
+      st.header('Acceso denegado')
+      client1.publish("IMIA","{'gesto': 'Acceso denegado'}",qos=0, retain=False)
+      time.sleep(0.2)  
