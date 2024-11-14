cdd04443618486df4debe014023ba6608f974c55
alejandrapalacuos <145595611+alejandrapalacuos@users.noreply.github.com>
2024-11-14 11:45:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ if img_file_buffer is not None:
     print(prediction)
     if prediction[0][0]>0.3:
       st.header('Abriendo')
-      client1.publish("IMIA","{'gesto': 'Abre'}",qos=0, retain=False)
+      client1.publish("vocecita","{'gesto': 'Abre'}",qos=0, retain=False)
       time.sleep(0.2)
     if prediction[0][1]>0.3:
       st.header('Acceso denegado')
-      client1.publish("IMIA","{'gesto': 'Acceso denegado'}",qos=0, retain=False)
+      client1.publish("vocecita","{'gesto': 'Acceso denegado'}",qos=0, retain=False)
       time.sleep(0.2)  
